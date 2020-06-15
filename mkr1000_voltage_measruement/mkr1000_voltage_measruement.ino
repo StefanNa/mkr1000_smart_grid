59e3348e283b5f026bca2c833278074efe4a2389
Stefan Nahstoll <steven_steven@hotmail.de>
2020-06-15 16:00:49 +0200

    starting to work on lowpass filter

--- mkr1000_voltage_measruement/mkr1000_voltage_measruement.ino
+++ mkr1000_voltage_measruement/mkr1000_voltage_measruement.ino
@@ -1,15 +1,21 @@
 const int sensorPin = A0;
-const int array_size = 1500;
+const int array_size = 3000;
 float values[array_size];
+//float values_[array_size];
 int timer_[array_size];
+int time__;
 unsigned long startMicros;
 unsigned long stopMicros;
+double cutoff=50.0/1000000.0;
+float RC;
+float alpha;
 
 float mean_int(int *x) {
+  float n = sizeof(x) / sizeof(x[0]);
   int sum=0;
-  for (int i = 0; i <= sizeof(x); i++){
+  for (int i = 0; i < n; i++){
     sum+=x[i];
-    return float(sum)/sizeof(x);
+    return float(sum)/float(n);
     }
   }
 
@@ -36,26 +42,43 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-startMicros=micros();
 
-for (int i = 0; i <= array_size; i++) {
+startMicros=micros();
+for (int i = 0; i < array_size; i++) {
+
     int sensorVal = analogRead(sensorPin);
-    float voltage = (sensorVal / 1024.0) * 3.3;
-    stopMicros=micros();
+    float voltage = (sensorVal / 4095.0) * 3.3;
     values[i]=voltage;
+    stopMicros=micros();
     timer_[i]=stopMicros-startMicros;
     startMicros=stopMicros;
   }
-  for (int i = 0; i <= array_size; i++) {
-    Serial.print("Voltage: ");
-    Serial.println(values[i]);
+  for (int b = 0; b < array_size-1; b++) {
+    RC=1/(2*3.1416*cutoff);
+//    Serial.print(cutoff,8);Serial.print("\t");Serial.println(RC,5);
+    alpha=timer_[b]/(RC+timer_[b]);
+//    alpha=0.01;
+//    Serial.print(timer_[b]);Serial.print("\t");Serial.println(alpha,5);
+    if (b==0){
+//      values_[b]=alpha*values[b+1]+(1-alpha)*values[b];
+      }
+    else {
+//    values_[b]=alpha*values[b+1]+(1-alpha)*values_[b-1];
+    }
+  }
+  for (int a = 0; a <= array_size; a++) {
+    
+//    Serial.print("Voltage: ");
+//    Serial.println(values[a]);
+//    Serial.print("Voltage_: ");
+    Serial.print(values[a],5);Serial.print(", ");Serial.println(values_[a]);
 //    Serial.print("time: ");
-//    Serial.println(timer_[i]);
+//    Serial.print(sizeof(timer_)/sizeof(timer_[0]));
+//    Serial.println(timer_[a]);
+    
     
   }
 //  Serial.println(mean_int(timer_));
-//  Serial.println(timer_[500]);
-
-  delay(3000);
-
+//  Serial.println(timer_[49]);
+  delay(1500);
  }
